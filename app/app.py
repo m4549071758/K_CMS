0ee1777d5987efcec46519a35b5dadeabf7cdfb5
m4549071758 <m4549071758@gmail.com>
2024-11-13 13:17:09 +0900

    app.py テンプレート作成

---
+++ app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required, create_refresh_token
+from flask_json import FlaskJSON
+from flasgger import Swagger
+from flasgger.utils import swag_from
+from hashlib import sha256
+from uuid_extensions import uuid7str
+from models.models import db_session
+from werkzeug.middleware.proxy_fix import ProxyFix
+import secrets
+
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(24)
+
+URL_PREFIX = "/api"
+
+jwt = JWTManager(app)
+app.config["JWT_SECRET_KEY"] = secrets.token_hex(24)
+app.config["JWT_ALGORITHM"] = "HS256"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
+
+FlaskJSON(app)
+app.config["JSON_AS_ASCII"] = False
+
+app.config["SWAGGER"] = {
+    "title": "ヘッドレスCMS API",
+    "uiversion": 3,
+    "version": "0.0.1",
+}
+swagger = Swagger(app)
