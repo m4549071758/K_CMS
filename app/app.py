b2235dd03f73afce0b4085028b5ed52f4f23f004
m4549071758 <m4549071758@gmail.com>
2024-11-14 12:19:58 +0900

    投稿API実装

--- app/app.py
+++ app/app.py
@@ -5,10 +5,11 @@ from flasgger import Swagger
 from flasgger.utils import swag_from
 from hashlib import sha256
 from uuid_extensions import uuid7str
-from models.models import db_session
+from models.models import db_session, Posts, Users
 from werkzeug.middleware.proxy_fix import ProxyFix
 from datetime import timedelta
 import secrets
+import os
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(24)
@@ -30,6 +31,73 @@ app.config["SWAGGER"] = {
     "version": "0.0.1",
 }
 swagger = Swagger(app)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+SAVE_FOLDER = "../blog/public/assets/blog"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "webp"}
+
+def jwt_unauthorized_loader_handler(reason):
+    return jsonify({"status": "error", "reason": reason}), 401
+
+@app.route(f"{URL_PREFIX}/posts/publish", methods=["POST"])
+@jwt_required
+@swag_from("swagger_yaml/posts_publish.yml")
+def publish_post():
+    current_user = get_jwt_identity()
+    data = request.json
+
+    post_id = uuid7str()
+    os.makedirs(f"./blog/_posts/{post_id}", exist_ok=True)
+
+    if "ogImage" not in data.files:
+        og_image = "/assets/blog/dynamic-routing/cover.webp"
+    else:
+        image = data.files["ogImage"]
+        os.makedirs(f"{SAVE_FOLDER}/{post_id}", exist_ok=True)
+        image.save(f"{SAVE_FOLDER}/{post_id}/{image.filename}")
+        # あとでwebp化処理
+        og_image = f"/assets/blog/{post_id}/{image.filename}"
+
+    title = data["title"]
+    excerpt = data["excerpt"]
+    tags = data["tags"]
+    date = data["date"]
+    markdown = data["markdown"]
+
+    new_tags = """"""
+    for tag in tags:
+        tags += f"  - {tag}\n"
+
+    new_markdown = f"""
+        ---
+        title: {title}
+        excerpt: {excerpt}
+        coverImage: {og_image}
+        ogImage: 
+          url: {og_image}
+        tags: 
+        {new_tags}
+        date: {date}
+        ---
+        {markdown}
+    """
+    
+    with open(f"./blog/_posts/{post_id}.md", "w") as f:
+        f.write(new_markdown)
+    
+    posts = Posts(id=post_id, title=title, tags=tags, date=date, user_id=current_user)
+
+    try:
+        db_session.add(posts)
+        db_session.commit()
+        db_session.close()
+        return jsonify({"message": "success"}), 200
+    except Exception as e:
+        db_session.rollback()
+        db_session.close()
+        return jsonify({"message": "Internal Server Error"}), 500
+
 
 # テストAPI
 @app.route(f"{URL_PREFIX}/ping", methods=["GET"])
