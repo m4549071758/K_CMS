8b06d77a5da887c893fb02229c46e918ac098fad
m4549071758 <m4549071758@gmail.com>
2025-07-09 23:18:44 +0900

    [fix]: CORSを修正

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -4,8 +4,29 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		origin := c.Request.Header.Get("Origin")
+
+		// 許可するオリジンのリスト
+		allowedOrigins := []string{
+			"http://localhost:3000",
+			"https://www.katori.dev",
+			"https://katori.dev",
+		}
+
+		// オリジンが許可リストに含まれているかチェック
+		allowed := false
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				allowed = true
+				break
+			}
+		}
+
+		if allowed {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
